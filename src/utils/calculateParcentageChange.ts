36732fa36d5623502df0b3ae5e628a10c9956cfd
LumenKrutin <Krutin.Trivedi@ibm.com>
2025-02-19 06:24:44 -0400

    removed log

--- src/utils/calculateParcentageChange.ts
+++ src/utils/calculateParcentageChange.ts
@@ -3,7 +3,6 @@ export const calculatePercentageChange = (currentValue: number, oldValue: number
   
   const change = ((currentValue - oldValue) / oldValue) * 100;
   const isPositive = isGoodIfIncreased ? change >= 0 : change >= 0;
-  console.log(`change >> ${change}, isGoodIfIncreased >> ${isGoodIfIncreased}, isPositive >> ${isPositive}`);
   const newQuantText = `${isPositive ? '⬆' : '⬇'} ${Math.abs(change).toFixed(2)}%`
   
   return {change: newQuantText, isPositive};
